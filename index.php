99684ea688bfdfca289222bc1ad7d03a34f016e5
vagon512 <vagon512@yandex.ru>
2021-05-07 12:16:22 +0300

    first version 2

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once "inc/page_struct.php";
 $page = new PageStruct("Помощник по ЯПам", "Добро пожаловать в помощник");
 $page->head();
 ?>
-<p>
+<p class=my_p>
 	Помощь по языкам программирования от IT TROJAN!
 </p>
 <?php
