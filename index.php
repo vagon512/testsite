46a44f856024b5fadbd459c0b8818372a0318584
vagon512 <vagon512@yandex.ru>
2021-05-07 18:34:35 +0300

    small change

--- index.php
+++ index.php
@@ -7,6 +7,19 @@ $page->head();
 <p class=my_p>
 	Помощь по языкам программирования от IT TROJAN!
 </p>
-<?php
+<?php 
+  echo "<form action = index.php method=post>
+        login:
+         <input type=text name=login maxlength=23 size = 25>
+         <br>
+         password:
+         <input type=password name=passwd size=20>
+         <br>
+         <input type=submit value=GO>
+         </form>";
+  if($_POST['login']){
+  	echo $_POST['login'];
+  }
+
 $page->foot();
 ?>
